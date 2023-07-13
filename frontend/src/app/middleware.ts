f871fe4264a31fb47b0140d3fa873eacb01a4d33
t77bsh <100529283+t77bsh@users.noreply.github.com>
2023-07-13 09:44:03 +0100

    added logs

--- frontend/src/app/middleware.ts
+++ frontend/src/app/middleware.ts
@@ -2,22 +2,11 @@ import { NextResponse, NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
   // Get a cookie
-  let sessionId = request.cookies.get("connect.sid")?.value;
+  let sessionIdCookie = request.cookies.get("connect.sid");
+  console.log(sessionIdCookie)
 
   // Get all cookies
-  // request.cookies.getAll()
-
-  // To change a cookie, first create a response
-  const response = NextResponse.next();
-
-  // Set a cookie
-  response.cookies.set("connect.sid", sessionId!);
-
-  // Setting a cookie with additional options
-  // response.cookies.set({
-  // 	name: 'myCookieName',
-  // 	value: 'some-value',
-  // 	httpOnly: true,
-  // })
-  return response;
+  const allCookies = request.cookies.getAll();
+  console.log(allCookies)
+  return NextResponse.next();
 }
