fc00da4b908e1ea4e94f39017ece72ca5a4e05f2
t77bsh <100529283+t77bsh@users.noreply.github.com>
2023-07-20 09:23:08 +0100

    categorised static and dynamic assets + favicon and robots.txt file

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -20,6 +20,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body className={sourceSans.className}>
         {children}
         <footer className="bg-gray-100">
